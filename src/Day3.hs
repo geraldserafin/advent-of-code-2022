782025e846e0084f551ddf4f8fb2d4ca59ee5552
geraldserafin <serafingerald@protonmail.com>
2022-12-03 17:18:54 +0100

    Solve day 3

---
+++ src/Day3.hs
@@ -0,0 +1,23 @@
+module Day3 (part1, part2) where
+
+import Data.List (intersect)
+import Data.List.Split (chunksOf)
+import Data.Char (isLower, ord)
+
+priority :: Char -> Int
+priority c
+  | isLower c = ord c - ord 'a' + 1
+  | otherwise = ord c - ord 'A' + 27
+
+-- assuming even length
+halve :: [a] -> [[a]]
+halve xs = chunksOf (div (length xs) 2) xs
+
+solve :: ([String] -> [[[Char]]]) -> String -> Int
+solve f = sum . map (priority . head . foldr1 intersect) . f . lines  
+
+part1 :: String -> Int
+part1 = solve (map halve)
+
+part2 :: String -> Int
+part2 = solve (chunksOf 3) 
