14bd41cd7b438136aa0f6f97f00c6291f0f16cba
geraldserafin <serafingerald@protonmail.com>
2022-12-10 19:37:05 +0100

    Improve day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,8 +1,8 @@
 module Day10 where
 
-import Text.Parsec (Parsec, many, anyChar)
 import AOC (solve, solveWrite)
 import Data.List.Split (chunksOf)
+import Text.Parsec (Parsec, many, anyChar)
 
 signal :: String -> Int
 signal "addx" = 0
@@ -16,15 +16,16 @@ cycles :: [Int] -> [Int]
 cycles = scanl1 (+) . ([1]++) 
 
 part1 :: String -> IO ()
-part1 = solve (sum . solution [20,60..220] . cycles) parser
+part1 = solve (sum . every 40 . drop 19 . zipWith (*) [1..] . cycles) parser
   where 
-    solution xs = map (\x -> (x*) . (!!(x-1)) $ xs)
+    every n [] = []
+    every n as = head as : (every n $ drop n as)
 
 part2 :: String -> IO ()
 part2 = solveWrite (toString . pixels . cycles) parser
   where 
-    pixels   = zipWith pixel (cycle [0..39]) 
-    toString = unlines . map (concat) . chunksOf 40 
+    pixels   = zipWith pixel $ cycle [0..39]
+    toString = unlines . map concat . chunksOf 40 
     pixel a b 
       | abs (a-b) <= 1 = "#"
       | otherwise      = " "
